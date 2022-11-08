2c2661af7ed2e81497b9da62ec0010d11a602270
maxmay94 <maxmay94@gmail.com>
2022-11-08 12:24:21 -0500

    learn about stats panel

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,91 +1,59 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
-const scene2 = new THREE.Scene()
-// scene.background = new THREE.Color(0xfff000)
-const camera1 = new THREE.PerspectiveCamera(
+
+const camera = new THREE.PerspectiveCamera(
     75,
-    // window.innerWidth / window.innerHeight,
-    200/200,
-    0.01,
+    window.innerWidth / window.innerHeight,
+    0.1,
     1000
 )
-const camera2 = new THREE.OrthographicCamera(-2,2,2,-2)
-const camera3 = new THREE.OrthographicCamera(-2,2,2,-2)
-const camera4 = new THREE.OrthographicCamera(-2,2,2,-2)
-
-camera1.position.z = 1
-camera2.position.y = 2
-camera2.lookAt(new THREE.Vector3())
-camera3.position.x = -2
-camera3.lookAt(new THREE.Vector3())
-camera4.position.z = 2
-
-const canvas1 = document.getElementById("c1") as HTMLCanvasElement
-const canvas2 = document.getElementById("c2") as HTMLCanvasElement
-const canvas3 = document.getElementById("c3") as HTMLCanvasElement
-const canvas4 = document.getElementById("c4") as HTMLCanvasElement
-
-const renderer1 = new THREE.WebGL1Renderer({canvas:canvas1})
-renderer1.setSize(200,200)
-
-const renderer2 = new THREE.WebGL1Renderer({canvas:canvas2})
-renderer2.setSize(200,200)
-
-const renderer3 = new THREE.WebGL1Renderer({canvas:canvas3})
-renderer3.setSize(200,200)
-
-const renderer4 = new THREE.WebGL1Renderer({canvas:canvas4})
-renderer4.setSize(200,200)
+camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera1, renderer1.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', render)
 
-const geometry = new THREE.TorusKnotGeometry()
-// const geometry = new THREE.BoxGeometry()
+const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
-const cube2 = new THREE.Mesh(geometry, material)
-cube.scale.x = .5
-cube.scale.y = .5
-cube.scale.z = .5
 scene.add(cube)
-scene2.add(cube2)
-
-// console.dir(scene)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera1.aspect = window.innerWidth / window.innerHeight
-    camera1.updateProjectionMatrix()
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
 function animate() {
     requestAnimationFrame(animate)
 
+    // stats.begin()
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
-
-    cube2.rotation.y += 0.01
+    // stats.end()
 
     render()
+    stats.update()
 }
 
 function render() {
-    renderer1.render(scene, camera1)
-    renderer2.render(scene, camera2)
-    renderer3.render(scene2, camera3)
-    renderer4.render(scene, camera4)
+    renderer.render(scene, camera)
 }
 
 animate()
+// render()
