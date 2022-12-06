d567ef7e476ba328dcac9268c18807d39e26ccc4
maxmay94 <maxmay94@gmail.com>
2022-12-06 11:39:42 -0500

    ray casting

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
@@ -12,142 +12,71 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.set(4, 4, 4)
+camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
+renderer.physicallyCorrectLights = true
+renderer.shadowMap.enabled = true
+renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 
-let mixer: THREE.AnimationMixer
-let modelReady = false
+// const material = new THREE.LineBasicMaterial({ color: 0xff0000 })
+// const points = new Array()
+// points.push( new THREE.Vector3( 0, 0, 0 ) )
+// points.push( new THREE.Vector3( 0, 0, .25 ) )
+// const geometry = new THREE.BufferGeometry().setFromPoints( points )
+// const line = new THREE.Line( geometry, material )
+// scene.add( line )
 
-const gltfLoader = new GLTFLoader()
+const arrowHelper = new THREE.ArrowHelper(
+    new THREE.Vector3(),
+    new THREE.Vector3(),
+    .25,
+    0xffff00)
+scene.add(arrowHelper)
 
-const dropzone = document.getElementById('dropzone') as HTMLDivElement
+const material = new THREE.MeshNormalMaterial()
 
-dropzone.ondragover = dropzone.ondragenter = function (evt) {
-    evt.preventDefault()
-}
-dropzone.ondrop = function (evt: DragEvent) {
-    evt.stopPropagation()
-    evt.preventDefault()
+// const boxGeometry = new THREE.BoxGeometry(.2, .2, .2)
+const coneGeometry = new THREE.ConeGeometry(.05, .2, 8)
 
-    //clear the scene
-    for (let i = scene.children.length - 1; i >= 0; i--) {
-        scene.remove(scene.children[i])
-    }
-    //clear the checkboxes
-    const myNode = document.getElementById('animationsPanel') as HTMLDivElement
-    while (myNode.firstChild) {
-        myNode.removeChild(myNode.lastChild as any)
-    }
+const raycaster = new THREE.Raycaster()
+const sceneMeshes: THREE.Object3D[] = []
 
-    const axesHelper = new THREE.AxesHelper(5)
-    scene.add(axesHelper)
-
-    const light1 = new THREE.DirectionalLight(new THREE.Color(0xffcccc))
-    light1.position.set(-1, 1, 1)
-    scene.add(light1)
-
-    const light2 = new THREE.DirectionalLight(new THREE.Color(0xccffcc))
-    light2.position.set(1, 1, 1)
-    scene.add(light2)
-
-    const light3 = new THREE.DirectionalLight(new THREE.Color(0xccccff))
-    light3.position.set(0, -1, 0)
-    scene.add(light3)
-
-    const files = (evt.dataTransfer as DataTransfer).files
-    const reader = new FileReader()
-    reader.onload = function () {
-        gltfLoader.parse(
-            reader.result as string,
-            '/',
-            (gltf: GLTF) => {
-                console.log(gltf.scene)
-
-                mixer = new THREE.AnimationMixer(gltf.scene)
-
-                console.log(gltf.animations)
-
-                if (gltf.animations.length > 0) {
-                    const animationsPanel = document.getElementById(
-                        'animationsPanel'
-                    ) as HTMLDivElement
-                    const ul = document.createElement('UL') as HTMLUListElement
-                    const ulElem = animationsPanel.appendChild(ul)
-
-                    gltf.animations.forEach((a: THREE.AnimationClip, i) => {
-                        const li = document.createElement('UL') as HTMLLIElement
-                        const liElem = ulElem.appendChild(li)
-
-                        const checkBox = document.createElement(
-                            'INPUT'
-                        ) as HTMLInputElement
-                        checkBox.id = 'checkbox_' + i
-                        checkBox.type = 'checkbox'
-                        checkBox.addEventListener('change', (e: Event) => {
-                            if ((e.target as HTMLInputElement).checked) {
-                                mixer
-                                    .clipAction((gltf as any).animations[i])
-                                    .play()
-                            } else {
-                                mixer
-                                    .clipAction((gltf as any).animations[i])
-                                    .stop()
-                            }
-                        })
-                        liElem.appendChild(checkBox)
-
-                        const label = document.createElement(
-                            'LABEL'
-                        ) as HTMLLabelElement
-                        label.htmlFor = 'checkbox_' + i
-                        label.innerHTML = a.name
-                        liElem.appendChild(label)
-                    })
-
-                    if (gltf.animations.length > 1) {
-                        const btnPlayAll = document.getElementById(
-                            'btnPlayAll'
-                        ) as HTMLButtonElement
-                        btnPlayAll.addEventListener('click', (e: Event) => {
-                            mixer.stopAllAction()
-                            gltf.animations.forEach(
-                                (a: THREE.AnimationClip) => {
-                                    mixer.clipAction(a).play()
-                                }
-                            )
-                        })
-
-                        btnPlayAll.style.display = 'block'
-                    }
-                } else {
-                    const animationsPanel = document.getElementById(
-                        'animationsPanel'
-                    ) as HTMLDivElement
-                    animationsPanel.innerHTML = 'No animations found in model'
-                }
-
-                scene.add(gltf.scene)
-
-                const bbox = new THREE.Box3().setFromObject(gltf.scene)
-                controls.target.x = (bbox.min.x + bbox.max.x) / 2
-                controls.target.y = (bbox.min.y + bbox.max.y) / 2
-                controls.target.z = (bbox.min.z + bbox.max.z) / 2
-
-                modelReady = true
-            },
-            (error) => {
-                console.log(error)
+const loader = new GLTFLoader()
+loader.load(
+    'models/monkey.glb',
+    function (gltf) {
+        gltf.scene.traverse(function (child) {
+            if ((child as THREE.Mesh).isMesh) {
+                const m = child as THREE.Mesh
+                m.receiveShadow = true
+                m.castShadow = true;
+                (m.material as THREE.MeshStandardMaterial).flatShading = true
+                sceneMeshes.push(m)
             }
-        )
+            if ((child as THREE.Light).isLight) {
+                const l = child as THREE.Light
+                l.castShadow = true
+                l.shadow.bias = -0.003
+                l.shadow.mapSize.width = 2048
+                l.shadow.mapSize.height = 2048
+            }
+        })
+        scene.add(gltf.scene)
+        //sceneMeshes.push(gltf.scene)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
     }
-    reader.readAsArrayBuffer(files[0])
-}
+)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -157,17 +86,78 @@ function onWindowResize() {
     render()
 }
 
+renderer.domElement.addEventListener('dblclick', onDoubleClick, false)
+renderer.domElement.addEventListener('mousemove', onMouseMove, false)
+
+function onMouseMove(event: MouseEvent) {
+    const mouse = {
+        x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+        y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+    }
+
+    // console.log(mouse)
+
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(sceneMeshes, false)
+
+    if (intersects.length > 0) {
+        // console.log(sceneMeshes.length + " " + intersects.length)
+        // console.log(intersects[0])
+        // console.log(intersects[0].object.userData.name + " " + intersects[0].distance + " ")
+        // console.log((intersects[0].face as THREE.Face).normal)
+        // line.position.set(0, 0, 0)
+        // line.lookAt((intersects[0].face as THREE.Face).normal)
+        // line.position.copy(intersects[0].point)
+
+        const n = new THREE.Vector3();
+        n.copy((intersects[0].face as THREE.Face).normal);
+        n.transformDirection(intersects[0].object.matrixWorld);
+
+        arrowHelper.setDirection(n);
+        arrowHelper.position.copy(intersects[0].point);
+    }
+}
+
+function onDoubleClick(event: MouseEvent) {
+    const mouse = {
+        x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+        y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+    }
+    raycaster.setFromCamera(mouse, camera)
+
+    const intersects = raycaster.intersectObjects(sceneMeshes, false)
+
+    if (intersects.length > 0) {
+
+        const n = new THREE.Vector3()
+        n.copy((intersects[0].face as THREE.Face).normal)
+        n.transformDirection(intersects[0].object.matrixWorld)
+
+        // const cube = new THREE.Mesh(boxGeometry, material)
+        const cube = new THREE.Mesh(coneGeometry, material)
+
+        cube.lookAt(n)
+        cube.rotateX(Math.PI / 2)
+        cube.position.copy(intersects[0].point)
+        cube.position.addScaledVector(n, .1)
+
+        scene.add(cube)
+        sceneMeshes.push(cube)
+    }
+}
+
 const stats = Stats()
 document.body.appendChild(stats.dom)
-
-const clock = new THREE.Clock()
 
 function animate() {
     requestAnimationFrame(animate)
 
     controls.update()
 
-    if (modelReady) mixer.update(clock.getDelta())
+    // if (sceneMeshes.length > 1) {
+    //     sceneMeshes[0].rotation.x += .002
+    // }
 
     render()
 
